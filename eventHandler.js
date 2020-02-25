cf7d774290cf671c711b231aee511907ee63d8e9
Aliyss Snow <silyas.bieri@lagomio.com>
2020-02-25 16:50:28 +0100

    Added Telegram and Discord Support. - telegram.js uses Telegraf - discord.js uses discord.js/discord.js - supported sending messages from clients

--- eventHandler.js
+++ eventHandler.js
@@ -1,4 +1,4 @@
-
+const converter = require('./../aliyssConverter/converterInput')
 
 exports.events = (_instance) => {
 	let client = _instance.client;
@@ -8,4 +8,18 @@ exports.events = (_instance) => {
 	})
 	
 	client.on('authenticated', _instance.saveAuth)
+	
+	client.on('message_create', msg => {
+		converter.message(msg, _instance)
+	})
+
+	client.on('message', msg => {
+		converter.message(msg, _instance)
+	});
+
+	client.on('text', msg => {
+		console.log(msg)
+	});
+	
+	_instance.disableEvents();
 }
--- eventInput.js
+++ eventInput.js
@@ -1,10 +1,14 @@
 
-exports.singleInstance = (_instance) => {
+let events = (_instance) => {
 	require("./eventHandler").events(_instance)
+}
+
+exports.singleInstance = (_instance) => {
+	events(_instance)
 }
 
 exports.multiInstances = (_instances) => {
 	for (let i = 0; i < _instances.length; i++) {
-		require("./eventHandler").events(_instances[i])
+		events(_instances[i])
 	}
 }
