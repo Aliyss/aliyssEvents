503b42593c3289d7eaad4215dd793fe2d4232f59
Aliyss Snow <silyas.bieri@lagomio.com>
2020-04-18 14:38:01 +0200

    Small redirection fix

--- eventHandler.js
+++ eventHandler.js
@@ -3,53 +3,91 @@ const commandInput = require("../aliyssCommands/commandInput");
 
 exports.events = (_instance) => {
 	let client = _instance.client;
+	
+	if (client.on) {
+		client.on('qr', qr => {
 
-	client.on('qr', qr => {
+		})
+
+		client.on('authenticated', _instance.saveAuth)
+
+		client.on('message_create', async msg => {
+			await messageControl(msg, _instance)
+		})
+
+		client.on('message', async msg => {
+			await messageControl(msg, _instance)
+		});
+
+		client.on('text', msg => {
+			console.log(msg)
+		});
+	}
+	
+	if (client.chat && client.chat.watchAllChannelsForNewMessages) {
+		(async () => {
+
+			const onMessage = async message => {
+				if (message.content.type !== 'text') {
+					return
+				}
+
+				if (!message.content.text.body.startsWith('!echo ')) {
+					return
+				}
+
+				await client.chat.send(message.conversationId, {
+					body: message.content.text.body.substr(6),
+				})
+			}
+
+			const onError = e => console.error(e)
+			
+			await client.chat.watchAllChannelsForNewMessages(onMessage, onError)
+		})();
 		
-	})
-	
-	client.on('authenticated', _instance.saveAuth)
-	
-	client.on('message_create', async msg => {
-		await messageControl(msg, _instance)
-	})
-
-	client.on('message', async msg => {
-		await messageControl(msg, _instance)
-	});
-
-	client.on('text', msg => {
-		console.log(msg)
-	});
+	}
 	
 	_instance.disableEvents();
 }
 
 const messageControl = async (msg, _instance) => {
 	let command = await converter.command(msg, _instance)
-	if (!command.isCommand
-		&& _instance.layout.nlp
-		&& _instance.layout.nlp.ignore
+	if (
+		!command.isPrefixed
+		&& command.layout.nlp
+		&& command.layout.nlp.ignore
 		&& (
-			(_instance.layout.nlp.ignore.channels && _instance.layout.nlp.ignore.channels.includes(command.channel.id)) ||
-			(_instance.layout.nlp.ignore.authors && _instance.layout.nlp.ignore.authors.includes(command.author.id))
-		)
-	) {
+			(command.layout.nlp.ignore.channels && command.layout.nlp.ignore.channels.includes(command.channel.id)) ||
+			(command.layout.nlp.ignore.authors && command.layout.nlp.ignore.authors.includes(command.author.id))
+		)) {
+		console.log('NLP is but disabled')
+		return;
+	} else if (
+		!command.isPrefixed
+		&& (
+			!command.layout.nlp ||
+			(!command.layout.nlp.no_prefix && !command.layout.nlp.on_command)
+		)) {
+		console.log('NLP is but no prefix')
 		return;
 	}
 	let returnMessage = await commandInput.command(command, _instance)
-	if (!command.isCommand && (!_instance.layout.nlp || !_instance.layout.nlp.no_prefix)) {
-		return;
-	}
 	if (!returnMessage || returnMessage.error) {
 		console.log(`[${_instance.id}] Error: ${returnMessage.error}`)
 		return;
 	}
-	let cmdMessage = converter.convertDefault(returnMessage, _instance)
+	let cmdMessage = converter.convertDefault(returnMessage, _instance, command)
 	if (!cmdMessage) {
 		return;
 	}
-	let logMessage = await converter.command(await command.send(cmdMessage), _instance)
+	let logMessage;
+	try {
+		logMessage = await converter.command(await command.send(cmdMessage), _instance)
+	} catch (e) {
+		console.error(e)
+	}
+	
 	_instance.logs.push(logMessage.id)
 	if (!logMessage) {
 		console.log(`[${_instance.id}] Error: ${msg}`)
