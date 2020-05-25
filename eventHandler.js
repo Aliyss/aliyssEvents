54745d1e7df5eea1c22b186c867588bf6d51e054
Aliyss Snow <silyas.bieri@lagomio.com>
2020-05-25 13:42:50 +0200

    Added Reaction Control - Added reactions

--- eventHandler.js
+++ eventHandler.js
@@ -12,11 +12,15 @@ exports.events = (_instance) => {
 		client.on('authenticated', _instance.saveAuth)
 
 		client.on('message_create', async msg => {
-			await messageControl(msg, _instance)
+			await messageControl(msg, _instance, 'message')
 		})
 
 		client.on('message', async msg => {
-			await messageControl(msg, _instance)
+			await messageControl(msg, _instance, 'message')
+		});
+
+		client.on('messageReactionAdd', async msg => {
+			await messageControl(msg, _instance, 'reaction')
 		});
 
 		client.on('text', msg => {
@@ -24,46 +28,23 @@ exports.events = (_instance) => {
 		});
 	}
 	
-	if (client.chat && client.chat.watchAllChannelsForNewMessages) {
-		(async () => {
-
-			const onMessage = async message => {
-				if (message.content.type !== 'text') {
-					return
-				}
-
-				if (!message.content.text.body.startsWith('!echo ')) {
-					return
-				}
-
-				await client.chat.send(message.conversationId, {
-					body: message.content.text.body.substr(6),
-				})
-			}
-
-			const onError = e => console.error(e)
-			
-			await client.chat.watchAllChannelsForNewMessages(onMessage, onError)
-		})();
-		
-	}
-	
 	_instance.disableEvents();
 }
 
-const messageControl = async (msg, _instance) => {
-	let command = await converter.command(msg, _instance)
+const messageControl = async (msg, _instance, type) => {
+	let command = await converter.command(msg, _instance, type)
 	if (
 		!command.isPrefixed
 		&& (
 			!command.layout.nlp ||
 			(!command.layout.nlp.no_prefix && !command.layout.nlp.on_command)
 		)) {
-		console.log('NLP is but no prefix')
 		return;
 	}
 	let returnMessage = await commandInput.command(command, _instance)
-	if (!returnMessage || returnMessage.error) {
+	if (!returnMessage) {
+		return;
+	} else if (returnMessage.error) {
 		console.log(`[${_instance.id}] Error: ${returnMessage.error}`)
 		return;
 	}
@@ -75,10 +56,10 @@ const messageControl = async (msg, _instance) => {
 	try {
 		logMessage = await converter.command(await command.send(cmdMessage), _instance)
 	} catch (e) {
-		// console.log(e)
+		console.log(e)
 	}
 	
-	if (!logMessage.id) {
+	if (!logMessage || !logMessage.id) {
 		return;
 	}
 	_instance.logs.push(logMessage.id)
